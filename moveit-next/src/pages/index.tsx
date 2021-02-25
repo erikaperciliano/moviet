cd9b68d0d9a9eebaf71dd8961f25cd2b04615b5c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-02-25 20:08:16 -0300

    add calculation of countdown

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,14 +1,20 @@
-import Head from 'next/head';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar/>
 
       <section>
@@ -19,7 +25,7 @@ export default function Home() {
         </div>
         
         <div>
-         
+         <ChallengeBox/>
         </div>
       </section>
   </div>
